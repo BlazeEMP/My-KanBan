2968a0e136355ec88d57d1d8968c71e95eb4bae8
BlazeEMP <blazelitanui@gmail.com>
2025-02-06 01:42:57 -0600

    fix rerouting on lougout

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,43 +3,45 @@ import { Link } from 'react-router-dom';
 import auth from '../utils/auth';
 
 const Navbar = () => {
-  const [ loginCheck, setLoginCheck ] = useState(false);
+    const [loginCheck, setLoginCheck] = useState(false);
 
-  const checkLogin = () => {
-    if(auth.loggedIn()) {
-      setLoginCheck(true);
-    }
-  };
+    const checkLogin = () => {
+        if (auth.loggedIn()) {
+            setLoginCheck(true);
+        }
+    };
 
-  useEffect(() => {
-    console.log(loginCheck);
-    checkLogin();
-  }, [loginCheck])
+    useEffect(() => {
+        console.log(loginCheck);
+        checkLogin();
+    }, [loginCheck])
 
-  return (
-    <div className='nav'>
-      <div className='nav-title'>
-        <Link to='/'>Krazy Kanban Board</Link>
-      </div>
-      <ul>
-      {
-        !loginCheck ? (
-          <li className='nav-item'>
-            <button type='button'>
-              <Link to='/login'>Login</Link>
-            </button>
-          </li>
-        ) : (
-          <li className='nav-item'>
-            <button type='button' onClick={() => {
-              auth.logout();
-            }}>Logout</button>
-          </li>
-        )
-      }
-      </ul>
-    </div>
-  )
+    return (
+        <div className='nav'>
+            <div className='nav-title'>
+                <Link to='/'>Krazy Kanban Board</Link>
+            </div>
+            <ul>
+                {
+                    !loginCheck ? (
+                        <li className='nav-item'>
+                            <button type='button'>
+                                <Link to='/login'>Login</Link>
+                            </button>
+                        </li>
+                    ) : (
+                        <li className='nav-item'>
+                            <button type='button' onClick={() => {
+                                auth.logout();
+                            }}>
+                                <Link to='/login'>Logout</Link>
+                            </button>
+                        </li>
+                    )
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default Navbar;
